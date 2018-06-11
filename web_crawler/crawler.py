ba87f6dee0678b189a0c66553556ce077e809ed5
mrthlinh <mrthlinh@gmail.com>
2018-06-11 01:42:43 -0500

    Player Info Crawler
    Crawl data of players from FIFA web

--- web_crawler/crawler.py
+++ web_crawler/crawler.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 import sys
+from unidecode import unidecode
+
 def retrieve_info(r,verbose=False):
     global df_index
     global squad_info
@@ -16,12 +18,12 @@ def retrieve_info(r,verbose=False):
     player_link = soup.find_all('td', attrs={"data-title": "Name"})
     # Build list of full url
     inner_link = [url_home+link.a['href'] for link in player_link] # List compprehension
-    
+
     # Iterate through the list of player and get info
     for link in inner_link:
-        progress = "\r"+str(inner_link.index(link)+1) + "/" + str(len(inner_link)) 
-        print(progress,end = '')
-        
+        progress = "\r"+str(inner_link.index(link)+1) + "/" + str(len(inner_link))
+        print(progress,end = ' ')
+
         sub_html_doc = requests.get(link).text
         sub_soup = BeautifulSoup(sub_html_doc,"lxml")
 
@@ -38,7 +40,11 @@ def retrieve_info(r,verbose=False):
             header = table.find('h3',attrs={"class": "panel-title"}).get_text()
             if (i == 0):
                 _ = header.split(" ")
-                player_info['Name'] = ' '.join(_[0:-2])
+                # player_info['Name'] = ' '.join(_[0:-2])
+                player_name = ' '.join(_[0:-2])
+                player_name = unidecode(player_name)
+                player_info['Name'] = player_name
+
                 player_info['OverRate'] = _[-2]
                 player_info['PotRate'] = _[-1]
             elif ((i == 1) & (len(tables) >= 9)):
@@ -71,8 +77,9 @@ def retrieve_info(r,verbose=False):
                 player_info[feature_name] = value
 
             i += 1
-#        if  (verbose):
-#            print(player_info['Name'])
+
+        if (verbose):
+           print(player_info['Name'])
 
         squad_info[df_index] = player_info
         df_index += 1
@@ -85,7 +92,7 @@ def retrieve_info(r,verbose=False):
 
 url_home = 'https://www.fifaindex.com'
 versions = ['fifa05_1','fifa06_2','fifa07_3','fifa08_4','fifa09_5','fifa10_6',
-'fifa11_7','fifa12_9','fifa13_11','fifa14_13','fifa15_14']
+'fifa11_7','fifa12_9','fifa13_11','fifa14_13','fifa15_14','fifa16_73','fifa17_173','']
 
 #version_id = 0
 version_id = int(sys.argv[1]) - 5
@@ -101,7 +108,7 @@ if len(sys.argv) >=3:
 if len(sys.argv) >=4:
     if (sys.argv[3] == 'T'):
         debug = True
-    
+
 df_index=0
 squad_info = {}
 next_page = start_page
